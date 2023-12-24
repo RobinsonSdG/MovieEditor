6ae51cc6e62cdf1a19984e002c7db3526eda9de3
RobinsonSdG <robsdg13@gmail.com>
2023-12-24 10:40:30 +0100

    first commit

---
+++ editor.py
@@ -0,0 +1,40 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip, vfx, CompositeAudioClip, afx
+
+# Chemins des fichiers vidéo et musique
+chemin_intro = "videos/030124.mp4"
+chemin_generique = "videos/WithNewLogo.mp4"
+chemin_principale = "videos/030124.mp4"
+
+chemin_voix = "musics/030124.wav"
+chemin_ronin = "musics/ronin2.mp3"
+chemin_musique_principale = "musics/ronin2.mp3"
+
+voix_intro = AudioFileClip(chemin_voix)
+voix_principale = AudioFileClip(chemin_voix)
+ronin = AudioFileClip(chemin_ronin)
+musique_principale = AudioFileClip(chemin_musique_principale)
+
+# Charger les vidéos et les musiques
+video_intro = VideoFileClip(chemin_intro).subclip(0,17.2).fx(vfx.fadeout, 1)
+audio_intro = CompositeAudioClip([ronin.subclip(0, int(video_intro.audio.duration)), voix_intro.subclip(0, int(video_intro.audio.duration))])
+video_intro = video_intro.set_audio(audio_intro)
+
+video_generique = VideoFileClip(chemin_generique).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
+
+video_principale = VideoFileClip(chemin_principale).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
+ronin_loop = afx.audio_loop(musique_principale, duration=video_principale.duration)
+audio_principale = CompositeAudioClip([ronin_loop, voix_principale])
+video_principale = video_principale.set_audio(audio_principale)
+video_principale = video_principale.subclip(17.7,video_principale.duration)
+
+# Ajouter la musique à l'introduction et à la partie principale
+# video_generique.without_audio()
+
+# # Créer les transitions entre les vidéos
+# transition = VideoFileClip("chemin_vers_transition.mp4")
+
+# Joindre les vidéos avec les transitions
+video_finale = concatenate_videoclips([video_intro, video_generique, video_principale])
+# video_finale = concatenate_videoclips([video_intro])
+# Sauvegarder la vidéo finale
+video_finale.write_videofile("finale030124.mp4")
